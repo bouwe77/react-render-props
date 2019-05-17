c9f348278fc1addda29b06f8f107e67f4543e995
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-05-17 23:35:16 +0200

    .

--- src/index.js
+++ src/index.js
@@ -4,18 +4,28 @@ import ReactDOM from "react-dom";
 function App() {
   return (
     <div>
-      <Toggle />
+      <Toggle render={renderToggleButton} />
+      <hr />
+      <Toggle render={renderToggleCheckbox} />
     </div>
   );
 }
 
-function Toggle() {
+function Toggle(props) {
   const [on, setOn] = useState(false);
 
+  return <>{props.render(() => setOn(!on), on)}</>;
+}
+
+function renderToggleButton(toggle, on) {
+  return <button onClick={() => toggle()}>{on ? "ON" : "OFF"}</button>;
+}
+
+function renderToggleCheckbox(toggle, on) {
   return (
     <>
-      <input type="checkbox" checked={on} onChange={() => setOn(!on)} />
-      {on ? "yes" : "no"}
+      <input type="checkbox" checked={on} onChange={() => toggle()} />
+      {on ? "ON" : "OFF"}
     </>
   );
 }
